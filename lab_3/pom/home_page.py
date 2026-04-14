ec87c51225210b46ef6fde3754f82250d99f7414
OnishchenkoDaria <daria.onishchenko.57@gmail.com>
2026-04-15 00:55:12 +0300

    feat: expanded homepage pom, shortened cahnge lang test

--- lab_3/pom/home_page.py
+++ lab_3/pom/home_page.py
@@ -1,3 +1,4 @@
+from selenium.common import TimeoutException
 from selenium.webdriver.common.by import By
 from lab_3.pom.base_page import BasePage
 from lab_3.utils.web_helpers import WebHelpers
@@ -8,16 +9,21 @@ class HomePage(BasePage):
 
     RECOMMENDED_SECTION = (By.XPATH, "//*[contains(text(), 'Рекомендовані товари')]")
     MENU_MASK = (By.ID, "menuMask")
-    ADD_TO_CART_BUTTONS = (By.CSS_SELECTOR, ".btn-addtocart")
+    ADD_TO_CART_BUTTON = (By.CSS_SELECTOR, ".btn-addtocart")
     MODAL_HEADING = (By.CSS_SELECTOR, ".modal-heading")
     CART_PRODUCT_NAMES = (By.CSS_SELECTOR, ".product-table-body-row > .name")
 
+    PROMO_TEXT = (By.CSS_SELECTOR, "p:nth-child(1)")
+    LANGUAGE_DROPDOWN = (By.CSS_SELECTOR, ".lang")
+    RUSSIAN_LANGUAGE_OPTION = (By.LINK_TEXT, "Русский")
+
     def open_home_page(self):
-        self.open(self.URL)
+        self.driver.get(self.URL)
+        WebHelpers.wait_for_page_ready(self.driver)
 
     def scroll_to_recommended_section(self):
-        section = self.wait_present(self.RECOMMENDED_SECTION)
-        self.scroll_to_element(section, block="start")
+        recommended = self.wait_present(self.RECOMMENDED_SECTION)
+        self.scroll_to_element(recommended, block="start")
 
     def close_menu_mask_if_present(self):
         WebHelpers.hide_overlay_if_present(
@@ -28,9 +34,9 @@ class HomePage(BasePage):
         )
 
     def click_first_add_to_cart_button(self):
-        button = self.wait_clickable(self.ADD_TO_CART_BUTTONS)
-        self.scroll_to_element(button, block="center")
-        self.js_click(button)
+        first_button = self.wait_clickable(self.ADD_TO_CART_BUTTON)
+        self.scroll_to_element(first_button, block="center")
+        self.js_click(first_button)
 
     def add_first_recommended_product_to_cart(self):
         self.scroll_to_recommended_section()
@@ -43,3 +49,22 @@ class HomePage(BasePage):
     def get_cart_products(self):
         self.wait_present(self.CART_PRODUCT_NAMES)
         return self.find_all(self.CART_PRODUCT_NAMES)
+
+    def get_promo_text(self):
+        return self.wait_visible(self.PROMO_TEXT).text
+
+    def open_language_dropdown(self):
+        dropdown = self.wait_clickable(self.LANGUAGE_DROPDOWN)
+        dropdown.click()
+
+    def is_russian_language_option_present(self):
+        try:
+            self.wait_visible(self.RUSSIAN_LANGUAGE_OPTION)
+            return True
+        except TimeoutException:
+            return False
+
+    def switch_language_to_russian(self):
+        russian_option = self.wait_clickable(self.RUSSIAN_LANGUAGE_OPTION)
+        russian_option.click()
+        WebHelpers.wait_for_page_ready(self.driver)
